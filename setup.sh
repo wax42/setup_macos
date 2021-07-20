513d721169bf62c5e800bea0368bc9538d29d520
vguerand <vguerand@vguerands-MacBook-Pro.local>
2021-07-20 15:56:13 +0200

    shooooortcuts

---
+++ setup.sh
@@ -0,0 +1,28 @@
+# Before setup developper command Line
+
+DevToolsSecurity -enable
+
+# install oh my zsh
+
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# Install brew
+
+rm -rf $HOME/.brew && git clone --depth=1 https://github.com/Homebrew/brew $HOME/.brew && export PATH=$HOME/.brew/bin:$PATH && brew update && echo "export PATH=$HOME/.brew/bin:$PATH" >> ~/.zshrc
+
+
+
+# Install software
+
+brew install --cask google-chrome   
+brew install --cask visual-studio-code
+brew install --cask iterm2
+brew install --cask slack
+brew install --cask discord
+
+brew install docker
+brew install python@3.9
+brew install node
+
+
+

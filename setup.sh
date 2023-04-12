689b86201546080a43d6c74841303c94b4cefe72
Victor Guerand <vguerand@Victors-MacBook-Pro.local>
2023-04-12 11:05:43 +0200

    Add Figma & Notion

--- setup.sh
+++ setup.sh
@@ -20,11 +20,16 @@ brew install --cask visual-studio-code
 brew install --cask iterm2
 brew install --cask slack
 brew install --cask discord
+brew install --cask figma
+brew install --cask notion
 brew install --cask dbeaver-community
+
 
 brew install docker
 brew install python@3.9
 brew install node
+
+
 
 
 
